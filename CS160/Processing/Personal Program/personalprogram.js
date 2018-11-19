413d8c903ea81f973fb3330890f70cba00c66e92
Travis <travis.ahner@hotmail.com>
2018-11-19 15:24:27 -0800

    Added all processing programs

---
+++ CS160/Processing/Personal Program/personalprogram.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(1920,1000);
+  }
+  
+  function draw() {
+    background(255);
+    fill(255, 204, 0);
+    ellipse(1000,500,400,400); 
+    fill(0);
+    ellipse(900,425,50,50);
+    ellipse(1100,425,50,50);
+    arc(1000,550,250,250,0,PI);
+    line(1120,550,890,550);
+  }
+  
