c9c89692c1cd3e76ed246e6e0881f5e1a59b2599
Travis <travis.ahner@hotmail.com>
2018-11-19 18:22:18 -0800

    Added comments, indentation for better readability

--- CS160/Processing/Personal Program/personalprogram.js
+++ CS160/Processing/Personal Program/personalprogram.js
@@ -3,13 +3,13 @@ function setup() {
 }
   
   function draw() {
-    background(255);
-    fill(255, 204, 0);
-    ellipse(1000,500,400,400); 
-    fill(0);
-    ellipse(900,425,50,50);
-    ellipse(1100,425,50,50);
-    arc(1000,550,250,250,0,PI);
-    line(1120,550,890,550);
+    background(255);    //Make background white
+    fill(255, 204, 0);  //Fill following shape with yellow
+      ellipse(1000,500,400,400); //Largest Circle
+    fill(0);            //Fill the following shapes with color black.
+      ellipse(900,425,50,50); //Left eye
+      ellipse(1100,425,50,50);  //Right eye
+      arc(1000,550,250,250,0,PI); //Creates bottom portion of mouth
+      line(1120,550,890,550); // Closes off mouth, allows for fill
 }
   
